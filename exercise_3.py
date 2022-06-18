7dcf25aaafa62ec5f012debb2b452bb4e6f91454
terfer <ferminca.tic@gmail.com>
2022-06-18 17:09:36 +0200

    Exercises 3 and 4 completed

---
+++ exercise_3.py
@@ -0,0 +1,13 @@
+import logging
+
+def run_timing(*arg):
+    run_time = input('Enter 10 km run time: ')
+    n_call = arg[1] + 1
+    if not run_time:
+        return (arg[0]/ arg[1]), (arg[1])
+    return run_timing(float(run_time) + arg[0], n_call)
+        
+   
+avg_time, n_runs = run_timing(0,0)
+
+print(f"Average of {avg_time}, over {n_runs} runs")
---
+++ exercise_4.py
@@ -0,0 +1,23 @@
+def hex_output(hex):
+    int_n = 0
+    for i,j in enumerate(reversed(hex[2:])):
+        if j not in ('a','b','c','d','e','f'):
+            int_n += int(j) * 16**i
+        else:
+            if j == 'a':
+                int_n += 10 * 16**i
+            elif j == 'b':
+                int_n += 11 * 16**i
+            elif j == 'c':
+                int_n += 12 * 16**i
+            elif j == 'd':
+                int_n += 13 * 16**i
+            elif j == 'e':
+                int_n += 14 * 16**i
+            else:
+                int_n += 15 * 16**i
+    return int_n
+
+
+
+
