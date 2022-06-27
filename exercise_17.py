40cff581ddac2968c18e2c43efd62c664812f0e1
terfer <ferminca.tic@gmail.com>
2022-06-28 00:42:09 +0200

    exercise_18

--- exercise_17.py
+++ exercise_17.py
@@ -1,6 +1,8 @@
 numbers = [1, 2, 3, 1, 2, 3, 4, 1]
 
-def how_many_different_numbers(num : list):
+
+def how_many_different_numbers(num: list):
     return len(set(num))
+
 
 print(how_many_different_numbers(numbers))
